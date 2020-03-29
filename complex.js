3f3874739285a3bfe53aa32a49ae85798133a3ba
kamilmaestro <ksdekamil@gmail.com>
2020-03-29 20:36:20 +0200

    second task

--- complex.js
+++ complex.js
@@ -1,27 +1,27 @@
-var Complex = /** @class */ (function () {
-    function Complex(real, imaginary) {
+"use strict";
+class Complex {
+    constructor(real, imaginary) {
         this.real = real;
         this.imaginary = imaginary;
     }
-    Complex.prototype.add = function (complex) {
+    add(complex) {
         return new Complex(this.real + complex.real, this.imaginary + complex.imaginary);
-    };
-    Complex.prototype.subtract = function (complex) {
+    }
+    subtract(complex) {
         return new Complex(this.real - complex.real, this.imaginary - complex.imaginary);
-    };
-    Complex.prototype.modulo = function () {
+    }
+    modulo() {
         return Math.sqrt(Math.pow(this.real, 2) + Math.pow(this.imaginary, 2));
-    };
-    Complex.prototype.toString = function () {
-        return this.real + ", " + this.imaginary;
-    };
-    return Complex;
-}());
-var fstComplex = new Complex(2, 5);
-var sndComplex = new Complex(4, 4);
-var sum = fstComplex.add(sndComplex);
-console.log("Sum: " + sum);
-var subtract = sum.subtract(fstComplex);
-console.log("Subtract: " + subtract);
-var mod = subtract.modulo();
-console.log("Modulo: " + mod);
+    }
+    toString() {
+        return `${this.real}, ${this.imaginary}`;
+    }
+}
+const fstComplex = new Complex(2, 5);
+const sndComplex = new Complex(4, 4);
+const sum = fstComplex.add(sndComplex);
+console.log(`Sum: ${sum}`);
+const subtract = sum.subtract(fstComplex);
+console.log(`Subtract: ${subtract}`);
+const mod = subtract.modulo();
+console.log(`Modulo: ${mod}`);
