e56a26cf7f2bf88c108c9fa4f02dc9d3689b5938
kamilmaestro <ksdekamil@gmail.com>
2020-04-04 11:43:08 +0200

    lab 6

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
-import {BlogComponent} from './components/blog/blog.component';
 import {ContactComponent} from './components/contact/contact.component';
+import {BlogItemDetailsComponent} from "./components/blog-item-details/blog-item-details.component";
+import {BlogHomeComponent} from "./components/blog-home/blog-home.component";
 
 
 const routes: Routes = [
   {
     path: 'blog',
-    component: BlogComponent
+    component: BlogHomeComponent
   },
   {
     path: '',
@@ -17,6 +18,10 @@ const routes: Routes = [
   {
     path: 'contact',
     component: ContactComponent
+  },
+  {
+    path: 'blog/detail/:id',
+    component: BlogItemDetailsComponent
   }
 ];
 
