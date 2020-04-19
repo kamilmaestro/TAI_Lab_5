28c7c533247be89493d8a28651d6e817b2aca9af
kamilmaestro <ksdekamil@gmail.com>
2020-04-19 19:44:29 +0200

    lab 7, 1 & 3 tasks

--- blog/src/app/components/blog-item-details/blog-item-details.component.ts
+++ blog/src/app/components/blog-item-details/blog-item-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {DataService} from "../../services/data.service";
 
 @Component({
   selector: 'blog-item-details',
@@ -7,12 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogItemDetailsComponent implements OnInit {
 
-  image = 'http://osnews.pl/wp-content/uploads/2016/06/it-grafika.jpg';
-  text = 'Tytuł';
+  public image = "";
+  public text = "";
+  public id: number;
 
-  constructor() { }
+  constructor(private dataService: DataService, private route: ActivatedRoute) {
+  }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    let id: string;
+    this.route.paramMap
+      .subscribe(params => {
+        id = params.get('id');
+      });
+
+    this.dataService.getById(id).subscribe(res => {
+      this.image = res['image'];
+      this.text = res['text'];
+    });
   }
 
 }
